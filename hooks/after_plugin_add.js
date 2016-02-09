6ffe152a6123f78d32669822b12d0f8b134e541a
Simon Prickett <simon@crudworks.org>
2016-02-09 07:38:55 -0800

    Adds plugin post add hook example.

---
+++ hooks/after_plugin_add.js
@@ -0,0 +1,4 @@
+#!/usr/bin/env node
+console.log('******************************************');
+console.log('* Hello from post plugin add hook script *');
+console.log('******************************************');
